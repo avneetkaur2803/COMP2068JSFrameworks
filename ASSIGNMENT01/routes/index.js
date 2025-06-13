ae241d8c91a0b5d42b023845e3caa33f6dd6619c
avneetkaur2803 <156447358+avneetkaur2803@users.noreply.github.com>
2025-06-13 16:56:21 -0400

    Added  header, navbar, and footer to layout.hbs
    Also changed local port to 3001 because of some errors.

--- ASSIGNMENT01/routes/index.js
+++ ASSIGNMENT01/routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// Home page
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('home', { title: 'Home' });
+});
+
+// About Me page
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About Me' });
+});
+
+// Projects page
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Projects' });
+});
+
+// Contact Me page
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact Me' });
 });
 
 module.exports = router;
